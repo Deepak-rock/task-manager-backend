ded79cc98c5e0e25bfc2b2bde3352abf2288410c
Deepak <deepakind777@gmail.com>
2025-06-19 15:34:34 +0530

    adding cors

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dataSource = require("./data-source");
 const routes = require("./routes");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3001
 
