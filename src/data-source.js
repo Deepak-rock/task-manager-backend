4731fb99b66f48dee11ef0adc5c653628e86e5be
Deepak <deepakind777@gmail.com>
2025-06-19 15:37:02 +0530

    fixing ssl

--- src/data-source.js
+++ src/data-source.js
@@ -13,12 +13,12 @@ module.exports = new DataSource({
   password: dbUrl.password,
   database: dbUrl.pathname.slice(1),
   synchronize: true,
-  logging: false, // Uncommand this in local development
-  /* ssl: true, // ssl is for deploy in render. While using in local you can remove ssl and add Logging: false.
+  /* logging: false, */ // Uncommand this in local development
+  ssl: true, // ssl is for deploy in render. While using in local you can remove ssl and add Logging: false.
   extra: {
     ssl: {
       rejectUnauthorized: false
     }
-  }, */
+  },
   entities: [Task],
 });
